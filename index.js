c88612eb77639c61665340a72dfe3ed5ff67c2ec
chomada <fede-cabj@live.com.ar>
2021-02-12 23:11:29 -0300

    cambio

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.use(cors());
 app.use(express.json({extended:true}));
 
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.use('/api/usuarios', require('./routes/usuarios'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/proyectos', require('./routes/proyectos'))
 
 
-app.listen(PORT,()=>{
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`)
+app.listen(port,'0.0.0.0',()=>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
 });
